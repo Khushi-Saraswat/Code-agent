7d373ccd058f8ef2f6cf6b9689b32f18c42d3d86
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-05-27 18:54:00 +0500

    added webhook api in server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/confi";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
+
+connectDB();
 
 const app = express();
-
 app.use(cors());
+
+// middlewares
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// clerk webhook api
+app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is working fine"));
 
