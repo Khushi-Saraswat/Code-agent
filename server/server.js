1a3bba61400c795f03db3f1dafd776fb318cc59a
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-05-31 17:15:34 +0500

    added changes

--- server/server.js
+++ server/server.js
@@ -1,29 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import "dotenv/config";
-// import connectDB from "./configs/db.js";
-
-// import { clerkMiddleware } from "@clerk/express";
-// import clerkWebhooks from "./controllers/clerkWebhooks.js";
-
-// connectDB();
-
-// const app = express();
-// app.use(cors());
-
-// // middlewares
-// app.use(express.json());
-// app.use(clerkMiddleware());
-
-// // clerk webhook api
-// app.use("/api/clerk", clerkWebhooks);
-
-// app.get("/", (req, res) => res.send("API is working fine"));
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => console.log(`server is running at ${PORT}`));
-
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
